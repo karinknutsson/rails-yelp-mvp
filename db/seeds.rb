5a646f96ed1ff2fec9411872ae3686c4d0dd8e9d
karin knutsson <karinmiriam@gmail.com>
2021-01-12 21:35:56 +0100

    finished everything except views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def generate_number
+  number = '+'
+  12.times do
+    digit = (1...10).to_a.sample
+    number += digit.to_s
+  end
+  number
+end
+
+10.times do
+  restaurant_name = Faker::BossaNova.song
+  address = Faker::Address.street_address + ', ' + Faker::Address.city
+  number = generate_number
+  category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+  Restaurant.create(name: restaurant_name, address: address, phone_number: number, category: category)
+end
