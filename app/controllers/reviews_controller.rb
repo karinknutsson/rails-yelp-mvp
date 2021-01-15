f2a5c560daaf57dfb9940a3db4f172f0cd85406b
karin knutsson <karinmiriam@gmail.com>
2021-01-15 17:36:03 +0100

    finished all the views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,26 @@
 class ReviewsController < ApplicationController
+  before_action :find_restaurant
+
+  def new
+    @restaurant = find_restaurant
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = find_restaurant
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
 end
